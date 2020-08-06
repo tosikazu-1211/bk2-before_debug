8ac9b6bd6e38bf26efaf9f22007838ba2a508acd
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-08-06 12:14:02 +0000

    kadai4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 	devise_for :users
-	resources :users,only: [:show,:index,:edit,:update]
+	resources :users,only: [:show,:index,:edit,:update] do
+		resources :relationships, only: [:create, :destroy]
+		get :follows, on: :member
+		get :followers, on: :member
+	end
 	resources :books do
 		resource :favorites, only: [:create, :destroy]
 		resources :book_comments, only: [:create, :destroy]
